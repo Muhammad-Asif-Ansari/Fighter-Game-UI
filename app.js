32b9b88d8a86ef5a5ba7c0b4ee462c13a3de2354
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-11-14 20:43:06 +0500

    Update Fighter UI
    Update Fighter UI

--- app.js
+++ app.js
@@ -1,59 +1,67 @@
-// ------ game logics -----
+// Game movement logic
+var forward1 = 0, forward2 = 0;
 
-var forward = 0;
-function abc() {
-  // console.log(event.key);
-  console.log(event.keyCode);
+function abc(event) {
+console.log(event.keyCode);
 
-  var character = document.getElementById("character");
-  if(event.keyCode === 65){
-    forward = forward + 10;
-    character.style.left = forward + "px"
+var character = document.getElementById("character");
+var characterWidth = character.offsetWidth;
+var screenWidth = window.innerWidth;
+
+if (event.keyCode === 65) { // 'A' key: Move right
+  if (forward1 + characterWidth + 10 <= screenWidth) { // Check if within right boundary
+    forward1 += 10;
+    character.style.left = forward1 + "px";
   }
-  if(event.keyCode === 66){
-    forward = forward - 10;
-    character.style.left = forward + "px"
+}
+if (event.keyCode === 66) { // 'B' key: Move left
+  if (forward1 - 10 >= 0) { // Check if within left boundary
+    forward1 -= 10;
+    character.style.left = forward1 + "px";
   }
-  if(event.keyCode === 67){
-    character.style.width = "300px"
-    character.style.height = "400px"
-    character.src = "./Images/chris-snap.gif"
-    
-  }
-  if(event.keyCode === 68){
-    character.style.width = "300px"
-    character.style.height = "400px"
-    character.src = "./Images/orochi-chris-kof97-stance.gif"
-    
-  }
+}
+if (event.keyCode === 67) { // 'C' key: Snap action
+  character.style.width = "300px";
+  character.style.height = "500px";
+  character.src = "./Images/chris-snap.gif";
+}
+if (event.keyCode === 68) { // 'D' key: Stance action
+  character.style.width = "300px";
+  character.style.height = "500px";
+  character.src = "./Images/orochi-chris-kof97-stance.gif";
+}
 }
 window.onkeyup = abc;
 
-var forward = 0;
-function xyz() {
-  // console.log(event.key);
-  console.log(event.keyCode);
+function xyz(event) {
+console.log(event.keyCode);
 
-  var character = document.getElementById("character1");
-  if(event.keyCode === 69){
-    forward = forward + 10;
-    character.style.right = forward + "px"
-  }
-  if(event.keyCode === 70){
-    forward = forward - 10;
-    character.style.right = forward + "px"
-  }
-  if(event.keyCode === 71){
-    character.style.width = "300px"
-    character.style.height = "450px"
-    character.src = "./Images/chris-snap.gif"
-    
-  }
-  if(event.keyCode === 72){
-    character.style.width = "300px"
-    character.style.height = "450px"
-    character.src = "./Images/orochi-chris-kof2002-stance.gif"
-    
+var character1 = document.getElementById("character1");
+var character1Width = character1.offsetWidth;
+var screenWidth = window.innerWidth;
+
+if (event.keyCode === 69) { // 'E' key: Move right
+  if (forward2 + character1Width + 10 <= screenWidth) { // Check if within right boundary
+    forward2 += 10;
+    character1.style.right = forward2 + "px";
   }
 }
+if (event.keyCode === 70) { // 'F' key: Move left
+  if (forward2 - 10 >= 0) { // Check if within left boundary
+    forward2 -= 10;
+    character1.style.right = forward2 + "px";
+  }
+}
+if (event.keyCode === 71) { // 'G' key: Snap action
+  character1.style.width = "300px";
+  character1.style.height = "600px";
+  character1.src = "./Images/chris-snap.gif";
+}
+if (event.keyCode === 72) { // 'H' key: Stance action
+  character1.style.width = "300px";
+  character1.style.height = "500px";
+  character1.src = "./Images/orochi-chris-kof2002-stance.gif";
+}
+}
+
 window.onkeydown = xyz;
